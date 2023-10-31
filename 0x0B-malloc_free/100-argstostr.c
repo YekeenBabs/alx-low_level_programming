347262b87d1bc8d4723f3637d0221a5167e52392
YekeenBabs <bankoleyekeen23@gmail.com>
2023-10-31 16:14:16 -0700

    101-strtow.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,18 +1,44 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
-char *concatenate_arguments();
+/**
+ * concatenate_arguments - Concatenate all the arguments.
+ * @av: Array of arguments.
+ *
+ * Return: Pointer to a new string, or NULL if it fails.
+ */
+char *concatenate_arguments(char **av)
+{
+	int i, j, k, len;
+	char *concatenated;
 
-int main(int argc, char *argv[]) {
-    // Call the concatenate_arguments function here
-    concatenate_arguments();
-    // Rest of your code
-    return 0;
-}
+	len = 0;
+	for (i = 0; av[i] != NULL; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+		len++; /* Add space for the space character */
+	}
 
-char *concatenate_arguments() {
-    // Implement the concatenate_arguments function here
-    char *result = NULL;
-    // Your implementation
-    return result;
+	concatenated = malloc(len * sizeof(char));
+	if (concatenated == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; av[i] != NULL; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			concatenated[k] = av[i][j];
+			k++;
+		}
+		concatenated[k] = ' '; /* Add a space character */
+		k++;
+	}
+
+	concatenated[k - 1] = '\0'; /* Null-terminate the final string */
+	return (concatenated);
 }
 
