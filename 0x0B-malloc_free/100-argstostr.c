55c51c18fd5bc245b09aec847274119fc6a95911
YekeenBabs <bankoleyekeen23@gmail.com>
2023-11-06 13:41:28 -0800

    more updaates

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-#include "myheader.h"
-#include "main_0.c"
 
 /** 
  *Function declaration
@@ -16,35 +14,36 @@ char *concatenate_arguments(char **av);
  */
 char *concatenate_arguments(char **av)
 {
-	int i, j, k, len;
-	char *concatenated;
+        int i, j, k, len;
+        char *concatenated;
 
-	len = 0;
-	for (i = 0; av[i] != NULL; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			len++;
-		}
-		len++; /* Add space for the space character */
-	}
+        len = 0;
+        for (i = 0; av[i] != NULL; i++)
+        {
+                for (j = 0; av[i][j] != '\0'; j++)
+                {
+                        len++;
+                }
+                len++; /* Add space for the space character */
+        }
 
-	concatenated = malloc(len * sizeof(char));
-	if (concatenated == NULL)
-		return (NULL);
+        concatenated = malloc(len * sizeof(char));
+        if (concatenated == NULL)
+                return (NULL);
 
-	k = 0;
-	for (i = 0; av[i] != NULL; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			concatenated[k] = av[i][j];
-			k++;
-		}
-		concatenated[k] = ' '; /* Add a space character */
-		k++;
-	}
+        k = 0;
+        for (i = 0; av[i] != NULL; i++)
+        {
+                for (j = 0; av[i][j] != '\0'; j++)
+                {
+                        concatenated[k] = av[i][j];
+                        k++;
+                }
+                concatenated[k] = ' '; /* Add a space character */
+                k++;
+        }
 
-	concatenated[k - 1] = '\0'; /* Null-terminate the final string */
-	return (concatenated);
+        concatenated[k - 1] = '\0'; /* Null-terminate the final string */
+        return (concatenated);
 }
+                        
--- 0x0B-malloc_free/myheader.h
+++
@@ -1,2 +0,0 @@
-char *argstostr(int ac, char **av);
-
